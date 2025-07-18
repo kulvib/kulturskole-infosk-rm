6552bd8038112abc7c39cc690fd8a6ebc99f1262
kulvib <here@viborg.dk>
2025-07-18 16:46:44 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
+import os
 import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    # Brug Render's PORT miljøvariabel hvis den findes, ellers 8000
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
