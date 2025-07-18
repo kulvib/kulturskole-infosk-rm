3acf49079a2dd8ee85cca37f3adc46cedb0711f4
kulvib <here@viborg.dk>
2025-07-18 14:07:27 +0200

    Delete client-agent/kiosk.sh

--- client-agent/kiosk.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-URL="https://www.kulturskolenviborg.dk/infoskaerm1"
-google-chrome --kiosk --noerrdialogs --disable-infobars --disable-session-crashed-bubble --incognito --disk-cache-dir=/dev/null --disable-popup-blocking $URL
