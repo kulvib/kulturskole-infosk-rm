9c0e1e2082832e77dccf88f43db5eb7d567d300b
kulvib <here@viborg.dk>
2025-07-18 12:51:43 +0200

    Update test_is_closed.py

--- backend/tests/test_is_closed.py
+++ backend/tests/test_is_closed.py
@@ -9,7 +9,6 @@ def test_is_closed_invalid_date():
     assert "error" in response.json()
 
 def test_is_closed_valid_date():
-    # Antag at der ikke er helligdage i holidays.json for denne dato
     response = client.get("/is_closed?date=2031-08-15")
     assert response.status_code == 200
     assert "closed" in response.json()
