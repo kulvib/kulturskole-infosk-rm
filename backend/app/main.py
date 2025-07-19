141aad31954016867783b2158d7c8aabbfa0388c
kulvib <here@viborg.dk>
2025-07-19 14:30:21 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 
 # ----------- FAST API-NØGLE TIL KLIENT-AUTOMATION -----------
-API_KEY = "API_KEY = "KulVib2025info"  # Sæt din egen nøgle!
+API_KEY = "KulVib2025info"  # Sæt din egen nøgle!
 
 # SECRET til JWT - SKIFT DETTE I PRODUKTION!
 SECRET_KEY = "supersecretkey123"
