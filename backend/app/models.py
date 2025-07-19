7d584f8d7c2786621a9c52a0451642612e4ecda3
kulvib <here@viborg.dk>
2025-07-19 17:20:26 +0200

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Boolean
 from .database import Base
 
 class Client(Base):
@@ -6,5 +6,11 @@ class Client(Base):
 
     id = Column(String, primary_key=True, index=True)
     name = Column(String, index=True)
-    display_name = Column(String, index=True)
+    display_name = Column(String)
     web_addr = Column(String)
+    ip = Column(String, nullable=True)
+    version = Column(String, nullable=True)
+    last_seen = Column(String, nullable=True)
+    uptime = Column(String, nullable=True)
+    online = Column(Boolean, default=False)
+    status = Column(String, default="pending")  # 'pending' eller 'approved'
