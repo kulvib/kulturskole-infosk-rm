4d9dde7e260489d9b835682cd7262dbee683dd57
kulvib <here@viborg.dk>
2025-07-27 19:36:50 +0200

    Update login.js

--- frontend/src/api/login.js
+++ frontend/src/api/login.js
@@ -2,12 +2,10 @@ import { api, setAuthToken } from "./api";
 
 export async function login(username, password) {
   try {
-    const response = await api.post("/login", { username, password });
-    const { access_token } = response.data;
-    setAuthToken(access_token);
-    return access_token;
-  } catch (error) {
-    // Smid fejlbesked fra backend eller standard fejl
-    throw error.response?.data?.detail || "Login fejlede";
+    const res = await api.post("/login", { username, password });
+    setAuthToken(res.data.access_token);
+    return res.data.access_token;
+  } catch (err) {
+    throw err.response?.data?.detail || "Login fejlede";
   }
 }
