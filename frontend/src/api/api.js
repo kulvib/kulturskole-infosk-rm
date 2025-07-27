716b811dc153c93b6f7e73a95c0a5f2907f67908
kulvib <here@viborg.dk>
2025-07-27 19:31:56 +0200

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/api";
 console.log("API_BASE_URL:", API_BASE_URL);
 
-// Opretter en axios-instans med baseURL fra miljøvariabel
 export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
@@ -11,7 +10,6 @@ export const api = axios.create({
   },
 });
 
-// Tilføj Authorization-header, hvis token findes
 export function setAuthToken(token) {
   if (token) {
     api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
