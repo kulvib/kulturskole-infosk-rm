c2753d17adc462bad4d1e666835cd23d37c6ada0
kulvib <here@viborg.dk>
2025-07-27 19:27:13 +0200

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/api";
+console.log("API_BASE_URL:", API_BASE_URL);
 
 // Opretter en axios-instans med baseURL fra miljøvariabel
 export const api = axios.create({
