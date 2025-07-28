096b46d34d426a1e87f7f8f49ac2bfbf35ce50a0
kulvib <here@viborg.dk>
2025-07-28 22:10:15 +0200

    Update ClientDetailsPageWrapper.js

--- frontend/src/components/ClientDetailsPageWrapper.js
+++ frontend/src/components/ClientDetailsPageWrapper.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import ClientDetailsPage from "./ClientDetailsPage";
 
-// Wrapper der giver clientId fra URL til ClientDetailsPage og videresender clients-prop fra parent (Dashboard)
 export default function ClientDetailsPageWrapper({ clients }) {
   const { clientId } = useParams();
   return <ClientDetailsPage clientId={clientId} clients={clients} />;
