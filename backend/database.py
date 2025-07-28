57d94dd3d1054ccc237aebdd96e150dcc2e14d59
kulvib <here@viborg.dk>
2025-07-28 18:40:29 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 from backend.models import Base
 
 DATABASE_URL = "sqlite:///./infoskaerm.db"
