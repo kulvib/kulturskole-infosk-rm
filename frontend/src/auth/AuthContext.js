82303e61afbb56f463beb0502378e70c3ab56eef
kulvib <here@viborg.dk>
2025-07-30 12:24:55 +0200

    Update AuthContext.js

--- frontend/src/auth/AuthContext.js
+++ frontend/src/auth/AuthContext.js
@@ -6,7 +6,7 @@ const FAKE_TOKEN = "PASTE_YOUR_JWT_TOKEN_HERE";
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [token] = useState(FAKE_TOKEN);
+  const [token] = useState(FAKE_TOKEN); // Brug evt. hardcoded token indtil login genindføres
   return (
     <AuthContext.Provider value={{ token }}>
       {children}
