68cb0de6e4f7c9a4aa680ea16d2f88f8676ee408
kulvib <here@viborg.dk>
2025-07-30 12:50:19 +0200

    Update AuthContext.js

--- frontend/src/auth/AuthContext.js
+++ frontend/src/auth/AuthContext.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from "react";
 
 // Midlertidigt hardcoded token til integration uden login
-const FAKE_TOKEN = "PASTE_YOUR_JWT_TOKEN_HERE";
+const FAKE_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTk5OTk5OTk5OX0.Fu6qHK7byNQ_GcanaXyvyma_d4fq_hq-wJPFuWXhyks";
 
 const AuthContext = createContext();
 
