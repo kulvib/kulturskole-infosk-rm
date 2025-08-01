44ca852517f749a23f185efee98c058419479474
kulvib <here@viborg.dk>
2025-08-01 13:06:24 +0200

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,19 +1,9 @@
-from sqlmodel import SQLModel, Field
 from typing import Optional
-
-class Client(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: Optional[str]
-    unique_id: Optional[str]
-    locality: Optional[str]
-    status: str = "pending"
-    ip_address: Optional[str]
-    mac_address: Optional[str]
-    isOnline: Optional[bool] = False
+from sqlmodel import SQLModel, Field
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    username: str
+    username: str = Field(index=True, unique=True)
     hashed_password: str
     role: str = "user"
     is_active: bool = True
