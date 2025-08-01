79c668e941536569042749b1cce4f392053b9da6
kulvib <here@viborg.dk>
2025-08-01 13:06:36 +0200

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,12 @@
-from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel import SQLModel, create_engine
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+import os
+from dotenv import load_dotenv
 
-engine = create_engine(sqlite_url, echo=False)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///database.db")
+engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
