f247148db0cc04965c4597ccbb409e5fc0c14641
kulvib <here@viborg.dk>
2025-08-01 13:18:48 +0200

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -10,7 +10,6 @@ engine = create_engine(DATABASE_URL, echo=True)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-# Dependency til FastAPI routes, så du får en session pr. request
 def get_session():
     with Session(engine) as session:
         yield session
