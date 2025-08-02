fc5d9818a4e2ae9a7ace49f9e213054f8e5e1f92
kulvib <here@viborg.dk>
2025-08-02 18:48:23 +0200

    Update holidays.py

--- backend/routers/holidays.py
+++ backend/routers/holidays.py
@@ -1,22 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
+from typing import List
+from ..db import get_session
 from ..models import Holiday
-from ..database import get_session
 
-router = APIRouter(prefix="/api/holidays", tags=["holidays"])
+router = APIRouter(tags=["holidays"])
 
-@router.get("/", response_model=list[Holiday])
+@router.get("/holidays/", response_model=List[Holiday])
 def get_holidays(session: Session = Depends(get_session)):
     return session.exec(select(Holiday)).all()
 
-@router.post("/", response_model=Holiday, status_code=status.HTTP_201_CREATED)
+@router.post("/holidays/", response_model=Holiday, status_code=status.HTTP_201_CREATED)
 def add_holiday(holiday: Holiday, session: Session = Depends(get_session)):
     session.add(holiday)
     session.commit()
     session.refresh(holiday)
     return holiday
 
-@router.delete("/{holiday_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/holidays/{holiday_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_holiday(holiday_id: int, session: Session = Depends(get_session)):
     holiday = session.get(Holiday, holiday_id)
     if not holiday:
