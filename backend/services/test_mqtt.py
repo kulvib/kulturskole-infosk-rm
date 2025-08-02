13b86e6f27c5a1cda81014c358f137a724ffb153
kulvib <here@viborg.dk>
2025-08-02 15:51:20 +0200

    Update test_mqtt.py

--- backend/services/test_mqtt.py
+++ backend/services/test_mqtt.py
@@ -2,4 +2,4 @@ from mqtt_service import connect, push_client_command
 
 if __name__ == "__main__":
     connect()
-    push_client_command("testclient", "ping", {"msg": "Hej fra backend!"})
+    push_client_command("testclient", "ping", {"msg": "Hej fra GitHub Actions!"})
