60541b2e74c22c0852ab798d80d64cf941e522e0
kulvib <here@viborg.dk>
2025-08-03 12:09:03 +0200

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, create_engine, Session
 
-DATABASE_URL = "sqlite:///database.db"  # Fastsat direkte
+DATABASE_URL = "postgresql://neondb_owner:npg_MTVCntKD8O2z@ep-raspy-math-a2qmxohl-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
