d59fe2d157bcb9c5e9d289cf9f65ef72414bab23
kulvib <here@viborg.dk>
2025-08-03 18:59:05 +0200

    Delete backend/mqtt_push.py

--- backend/mqtt_push.py
+++
@@ -1,7 +0,0 @@
-import paho.mqtt.publish as publish
-import json
-
-def push_command(client_id, command, payload=None):
-    topic = f"client/{client_id}/commands"
-    msg = json.dumps({"command": command, "payload": payload})
-    publish.single(topic, msg, hostname="localhost")
