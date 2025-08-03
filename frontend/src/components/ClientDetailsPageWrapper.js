50b774d6a9a732aa598430de9209fe5f7ff258cb
kulvib <here@viborg.dk>
2025-08-03 23:18:43 +0200

    Update ClientDetailsPageWrapper.js

--- frontend/src/components/ClientDetailsPageWrapper.js
+++ frontend/src/components/ClientDetailsPageWrapper.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import ClientDetailsPage from "./ClientDetailsPage";
 import { useAuth } from "../auth/authcontext";
 import { getClient } from "../api";
+import ClientDetailsPage from "./ClientDetailsPage";
 
 // Wrapper henter altid frisk klientdata fra API, og bruger token fra context
-export default function ClientDetailsPageWrapper({ clients }) {
+export default function ClientDetailsPageWrapper() {
   const { clientId } = useParams();
   const { token } = useAuth();
   const [client, setClient] = useState(null);
