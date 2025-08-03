59093d24a2006f87e68ff772ee48ed96d095c015
kulvib <here@viborg.dk>
2025-08-03 10:30:16 +0200

    Update ws_manager.py

--- backend/ws_manager.py
+++ backend/ws_manager.py
@@ -9,6 +9,6 @@ async def websocket_endpoint(websocket: WebSocket):
     connected_websockets.append(websocket)
     try:
         while True:
-            await websocket.receive_text()  # holder forbindelsen åben
+            await websocket.receive_text()
     except WebSocketDisconnect:
         connected_websockets.remove(websocket)
