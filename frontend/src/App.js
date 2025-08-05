a5e6b2f6bc40d434448fbc5bf722b01b57bf9e9f
kulvib <here@viborg.dk>
2025-08-05 21:21:45 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import ClientInfoPage from "./components/ClientInfoPage";
-import HolidaysPage from "./components/HolidaysPage";
 import ClientDetailsPageWrapper from "./components/ClientDetailsPageWrapper";
 import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
@@ -10,6 +9,7 @@ import NotFound from "./components/NotFound";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import CalendarView from "./components/CalendarView";
 import { AuthProvider } from "./auth/authcontext";
+import AdminPage from "./components/AdminPage"; // NYT!
 
 export default function App() {
   return (
@@ -28,8 +28,9 @@ export default function App() {
             <Route index element={<HomePage />} />
             <Route path="clients" element={<ClientInfoPage />} />
             <Route path="clients/:clientId" element={<ClientDetailsPageWrapper />} />
-            <Route path="holidays" element={<HolidaysPage />} />
+            {/* <Route path="holidays" element={<HolidaysPage />} />  <-- SLET DENNE LINJE */}
             <Route path="calendar" element={<CalendarView />} />
+            <Route path="administration" element={<AdminPage />} /> {/* NYT! */}
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
