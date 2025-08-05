8e26dc40ba2b065cd51c1bf36e411d66826e20a8
kulvib <here@viborg.dk>
2025-08-05 22:19:15 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
 import NotFound from "./components/NotFound";
 import ProtectedRoute from "./auth/ProtectedRoute";
-import CalendarView from "./components/CalendarView";
+import CalendarPage from "./components/CalendarPage"; // <-- Ændret her
 import { AuthProvider } from "./auth/authcontext";
 import AdminPage from "./components/AdminPage"; // NYT!
 
@@ -29,7 +29,7 @@ export default function App() {
             <Route path="clients" element={<ClientInfoPage />} />
             <Route path="clients/:clientId" element={<ClientDetailsPageWrapper />} />
             {/* <Route path="holidays" element={<HolidaysPage />} />  <-- SLET DENNE LINJE */}
-            <Route path="calendar" element={<CalendarView />} />
+            <Route path="calendar" element={<CalendarPage />} /> {/* <-- Ændret her */}
             <Route path="administration" element={<AdminPage />} /> {/* NYT! */}
           </Route>
           <Route path="*" element={<NotFound />} />
