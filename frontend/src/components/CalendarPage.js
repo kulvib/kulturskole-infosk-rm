393ae330d3f5a263d325d97d8f5f0c08cd4d5b79
kulvib <here@viborg.dk>
2025-08-06 15:59:49 +0200

    Update CalendarPage.js

--- frontend/src/components/CalendarPage.js
+++ frontend/src/components/CalendarPage.js
@@ -7,7 +7,7 @@ import RefreshIcon from "@mui/icons-material/Refresh";
 import { getClients, saveMarkedDays, getMarkedDays } from "../api";
 import { useAuth } from "../auth/authcontext";
 import DateTimeEditDialog from "./DateTimeEditDialog";
-import ClientSelector from "./ClientSelector"; // Integreret
+import ClientSelector from "./ClientSelector"; // Kun checkboxes og søgning
 
 const monthNames = [
   "August", "September", "Oktober", "November", "December",
