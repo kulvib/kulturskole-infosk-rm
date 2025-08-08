318c1ba6e7e91817141bcc107f3d1cc0144418e3
kulvib <here@viborg.dk>
2025-08-08 10:00:52 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,10 @@ import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
 import NotFound from "./components/NotFound";
 import ProtectedRoute from "./auth/ProtectedRoute";
-import CalendarPage from "./components/CalendarPage"; // <-- Ændret her
+import CalendarPage from "./components/CalendarPage";
 import { AuthProvider } from "./auth/authcontext";
-import AdminPage from "./components/AdminPage"; // NYT!
+import AdminPage from "./components/AdminPage";
+import RemoteDesktop from "./components/RemoteDesktop"; // NYT!
 
 export default function App() {
   return (
@@ -28,10 +29,11 @@ export default function App() {
             <Route index element={<HomePage />} />
             <Route path="clients" element={<ClientInfoPage />} />
             <Route path="clients/:clientId" element={<ClientDetailsPageWrapper />} />
-            {/* <Route path="holidays" element={<HolidaysPage />} />  <-- SLET DENNE LINJE */}
-            <Route path="calendar" element={<CalendarPage />} /> {/* <-- Ændret her */}
-            <Route path="administration" element={<AdminPage />} /> {/* NYT! */}
+            <Route path="calendar" element={<CalendarPage />} />
+            <Route path="administration" element={<AdminPage />} />
           </Route>
+          {/* NY ROUTE: Remote Desktop, åbnes i ny fane */}
+          <Route path="/remote-desktop/:clientId" element={<RemoteDesktop />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
