765e3274bca94fa6b99c4f3bae7b82c03d5ff69c
kulvib <here@viborg.dk>
2025-08-09 17:48:36 +0200

    Create guacamole-init-postgresql.sql

---
+++ backend/service2_RemoteDesktop/guacamole-init-postgresql.sql
@@ -0,0 +1,5 @@
+-- Officielt Guacamole PostgreSQL init-script: hent her og kør i din database!
+-- https://downloads.apache.org/guacamole/1.5.4/binary/guacamole-auth-jdbc-postgresql-1.5.4.sql
+
+-- Eksempel på at køre scriptet:
+-- psql -U neondb_owner -d guacamole_RemoteDesktop -f guacamole-auth-jdbc-postgresql-1.5.4.sql
