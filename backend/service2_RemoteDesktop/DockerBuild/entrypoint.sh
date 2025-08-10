d571c4025118cd7cd5299a0fa8ff7d34e0cfa0f9
kulvib <here@viborg.dk>
2025-08-10 14:23:48 +0200

    Delete backend/service2_RemoteDesktop/DockerBuild/entrypoint.sh

--- backend/service2_RemoteDesktop/DockerBuild/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Entrypoint.sh startes..."
-envsubst < /etc/guacamole/guacamole.properties > /etc/guacamole/guacamole.properties.subst
-mv /etc/guacamole/guacamole.properties.subst /etc/guacamole/guacamole.properties
-ls -l /etc/guacamole/extensions/
-ls -l /etc/guacamole/
-exec /usr/local/tomcat/bin/catalina.sh run
