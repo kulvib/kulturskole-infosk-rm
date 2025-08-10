2a72119b1a448419123c08263b95985c4b89128e
kulvib <here@viborg.dk>
2025-08-10 14:19:42 +0200

    Update entrypoint.sh

--- backend/service2_RemoteDesktop/DockerBuild/entrypoint.sh
+++ backend/service2_RemoteDesktop/DockerBuild/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# Erstat miljøvariabler i guacamole.properties
+echo "Entrypoint.sh startes..."
 envsubst < /etc/guacamole/guacamole.properties > /etc/guacamole/guacamole.properties.subst
 mv /etc/guacamole/guacamole.properties.subst /etc/guacamole/guacamole.properties
+ls -l /etc/guacamole/extensions/
+ls -l /etc/guacamole/
 exec /usr/local/tomcat/bin/catalina.sh run
