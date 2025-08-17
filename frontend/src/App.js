b95bc0d74b652fc9aa1afde8e6b6f6232f465981
kulvib <here@viborg.dk>
2025-08-17 20:47:30 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,35 +8,32 @@ import HomePage from "./components/HomePage";
 import NotFound from "./components/NotFound";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import CalendarPage from "./components/CalendarPage";
-import { AuthProvider } from "./auth/authcontext";
 import AdminPage from "./components/AdminPage";
 import RemoteDesktop from "./components/RemoteDesktop"; // NYT!
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<HomePage />} />
-            <Route path="clients" element={<ClientInfoPage />} />
-            <Route path="clients/:clientId" element={<ClientDetailsPageWrapper />} />
-            <Route path="calendar" element={<CalendarPage />} />
-            <Route path="administration" element={<AdminPage />} />
-          </Route>
-          {/* NY ROUTE: Remote Desktop, åbnes i ny fane */}
-          <Route path="/remote-desktop/:clientId" element={<RemoteDesktop />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<HomePage />} />
+          <Route path="clients" element={<ClientInfoPage />} />
+          <Route path="clients/:clientId" element={<ClientDetailsPageWrapper />} />
+          <Route path="calendar" element={<CalendarPage />} />
+          <Route path="administration" element={<AdminPage />} />
+        </Route>
+        {/* NY ROUTE: Remote Desktop, åbnes i ny fane */}
+        <Route path="/remote-desktop/:clientId" element={<RemoteDesktop />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
