5165f6b636cb0c5d018d5d8503c5fb76e1c0f44e
kulvib <here@viborg.dk>
2025-08-27 19:34:00 +0200

    Update models.py

--- backend/service1/models.py
+++ backend/service1/models.py
@@ -45,7 +45,7 @@ class Client(ClientBase, table=True):
     pending_shutdown: Optional[bool] = False
     chrome_color: Optional[str] = None
     pending_chrome_action: Optional[ChromeAction] = Field(default=ChromeAction.NONE)
-    school_id: Optional[int] = Field(default=None, foreign_key="school.id")  # relation til skole
+    school_id: Optional[int] = Field(default=None, foreign_key="school.id")  # NYT: relation til School
 
 class ClientCreate(ClientBase):
     sort_order: Optional[int] = None
@@ -59,7 +59,7 @@ class ClientCreate(ClientBase):
     chrome_status: Optional[str] = None
     chrome_color: Optional[str] = None
     pending_chrome_action: Optional[ChromeAction] = ChromeAction.NONE
-    school_id: Optional[int] = None  # relation til skole
+    school_id: Optional[int] = None  # NYT
 
 class ClientUpdate(SQLModel):
     locality: Optional[str] = None
@@ -77,7 +77,7 @@ class ClientUpdate(SQLModel):
     chrome_last_updated: Optional[datetime] = None
     chrome_color: Optional[str] = None
     pending_chrome_action: Optional[ChromeAction] = None
-    school_id: Optional[int] = None  # relation til skole
+    school_id: Optional[int] = None  # NYT
 
 class CalendarMarking(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
