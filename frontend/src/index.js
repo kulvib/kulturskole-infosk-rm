11840fc5248cb4c0d00025e58c8c8639c96d5aa9
kulvib <here@viborg.dk>
2025-09-19 13:34:06 +0200

    Wrap App component with BrowserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthProvider } from "./auth/authcontext";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
