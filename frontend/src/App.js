3ae3502342072e546423bb5ed25fd801df9531c3
kulvib <here@viborg.dk>
2025-09-19 13:34:30 +0200

    Remove BrowserRouter and simplify routing structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import ClientInfoPage from "./components/ClientInfoPage";
 import ClientDetailsPageWrapper from "./components/ClientDetailsPage/ClientDetailsPageWrapper";
@@ -14,27 +14,25 @@ import RemoteDesktop from "./components/RemoteDesktop";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<HomePage />} />
-          <Route path="clients" element={<ClientInfoPage />} />
-          <Route path="clients/:clientId" element={<ClientDetailsPageWrapper />} />
-          <Route path="calendar" element={<CalendarPage />} />
-          <Route path="administration" element={<AdminPage />} />
-          {/* <Route path="livestream-test" element={<LivestreamTest />} /> FJERNET */}
-        </Route>
-        <Route path="/remote-desktop/:clientId" element={<RemoteDesktop />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<HomePage />} />
+        <Route path="clients" element={<ClientInfoPage />} />
+        <Route path="clients/:clientId" element={<ClientDetailsPageWrapper />} />
+        <Route path="calendar" element={<CalendarPage />} />
+        <Route path="administration" element={<AdminPage />} />
+        {/* <Route path="livestream-test" element={<LivestreamTest />} /> FJERNET */}
+      </Route>
+      <Route path="/remote-desktop/:clientId" element={<RemoteDesktop />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
