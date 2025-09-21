1f991d97b6ca4f8fbc59da60e5d936a5fc52b7f5
kulvib <here@viborg.dk>
2025-09-21 13:23:58 +0200

    Increase maxWidth of AdminPage component

--- frontend/src/components/AdminPage/AdminPage.js
+++ frontend/src/components/AdminPage/AdminPage.js
@@ -7,18 +7,20 @@ export default function AdminPage() {
   const [activeTab, setActiveTab] = useState("school");
 
   return (
-    <Box sx={{ maxWidth: 1000, mx: "auto", mt: 4 }}>
+    <Box sx={{ maxWidth: 1200, mx: "auto", mt: 4 }}>
       <Paper sx={{ p: 2, mb: 3, textAlign: "center" }}>
         <ButtonGroup variant="contained">
           <Button
             onClick={() => setActiveTab("school")}
             color={activeTab === "school" ? "primary" : "inherit"}
+            sx={{ fontWeight: activeTab === "school" ? 700 : 400 }}
           >
             Skoleadministration
           </Button>
           <Button
             onClick={() => setActiveTab("user")}
             color={activeTab === "user" ? "primary" : "inherit"}
+            sx={{ fontWeight: activeTab === "user" ? 700 : 400 }}
           >
             Brugeradministration
           </Button>
