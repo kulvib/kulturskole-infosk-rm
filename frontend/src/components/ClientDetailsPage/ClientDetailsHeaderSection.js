ed264405d84e7c0fa58d1d1ac52f25da6a44ee2c
kulvib <here@viborg.dk>
2025-10-07 20:52:39 +0200

    Adjust margin for status dot in ClientDetailsHeader

--- frontend/src/components/ClientDetailsPage/ClientDetailsHeaderSection.js
+++ frontend/src/components/ClientDetailsPage/ClientDetailsHeaderSection.js
@@ -82,7 +82,7 @@ function ChromeStatusBadge({ status, color, isMobile = false }) {
           fontWeight: 400,
           textTransform: "none",
           fontSize: isMobile ? 12 : undefined,
-          mr: "2px", // <-- statusdot 10px tættere på teksten
+          mr: "3px", // <-- statusdot 3px tættere på teksten
         }}
       >
         {text}
