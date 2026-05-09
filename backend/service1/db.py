e454b61c0cf6b8977eef4d4ba8b042f01c4bd110
kulvib <here@viborg.dk>
2026-05-10 01:31:00 +0200

    Update db.py

--- backend/service1/db.py
+++ backend/service1/db.py
@@ -3,8 +3,14 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///database.db")
-engine = create_engine(DATABASE_URL, echo=True)
+
+# echo=False i produktion - undgår at printe SQL + data i logs
+engine = create_engine(
+    DATABASE_URL,
+    echo=os.getenv("ENVIRONMENT") != "production"
+)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
