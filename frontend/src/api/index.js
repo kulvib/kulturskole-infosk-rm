b7a1b2c38ea7089ffbae591f274ed944a48a95b1
kulvib <here@viborg.dk>
2026-05-18 18:18:26 +0200

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,3 +1,3 @@
 // src/api/index.js
-// Re-eksporterer alt fra api.js for at understøtte "from '../../api'" imports
+// Re-eksporterer alt fra api.js for at understøtte "from ../../api" imports
 export * from "./api";
